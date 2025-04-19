fa61d7092f13c5defda521e2e22cb64774e50cbc
Neo Zhang Jianyu <jianyu.zhang@intel.com>
2025-04-19 10:55:56 +0800

    restore c code, correct build package

--- ml/backend/ggml/ggml/src/ggml-backend-reg.cpp
+++ ml/backend/ggml/ggml/src/ggml-backend-reg.cpp
@@ -225,7 +225,6 @@ struct ggml_backend_registry {
         dl_handle_ptr handle { dl_load_library(path) };
         if (!handle) {
             if (!silent) {
-                GGML_LOG_ERROR("Error: %s\n",  dlerror());
                 GGML_LOG_ERROR("%s: failed to load %s\n", __func__, path_to_string(path).c_str());
             }
             return nullptr;
@@ -466,8 +465,9 @@ static ggml_backend_reg_t ggml_backend_load_best(const char * name, bool silent,
     // enumerate all the files that match [lib]ggml-name-*.[so|dll] in the search paths
      // TODO: search system paths
     namespace fs = std::filesystem;
-    std::string file_prefix = backend_filename_prefix() + name;
+    std::string file_prefix = backend_filename_prefix() + name + "-";
     std::vector<fs::path> search_paths;
+
     if (user_search_path == nullptr) {
         search_paths.push_back(fs::current_path());
         search_paths.push_back(get_executable_path());
@@ -483,7 +483,6 @@ static ggml_backend_reg_t ggml_backend_load_best(const char * name, bool silent,
             continue;
         }
         fs::directory_iterator dir_it(search_path, fs::directory_options::skip_permission_denied);
-
         for (const auto & entry : dir_it) {
             if (entry.is_regular_file()) {
                 std::string filename = entry.path().filename().string();
@@ -491,7 +490,6 @@ static ggml_backend_reg_t ggml_backend_load_best(const char * name, bool silent,
                 if (filename.find(file_prefix) == 0 && ext == backend_filename_suffix()) {
                     dl_handle_ptr handle { dl_load_library(entry.path()) };
                     if (!handle) {
-                        GGML_LOG_ERROR("Error: %s\n",  dlerror());
                         GGML_LOG_ERROR("%s: failed to load %s\n", __func__, path_to_string(entry.path()).c_str());
                         continue;
                     }
@@ -512,6 +510,7 @@ static ggml_backend_reg_t ggml_backend_load_best(const char * name, bool silent,
             }
         }
     }
+
     if (best_score == 0) {
         // try to load the base backend
         for (const auto & search_path : search_paths) {
@@ -522,6 +521,7 @@ static ggml_backend_reg_t ggml_backend_load_best(const char * name, bool silent,
         }
         return nullptr;
     }
+
     return get_reg().load_backend(best_path, silent);
 }
 
--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -44,7 +44,7 @@ if echo $PLATFORM | grep "," > /dev/null ; then
         tar c -C ./dist/linux_arm64 --exclude cuda_jetpack5 --exclude cuda_jetpack6 . | pigz -9vc >./dist/ollama-linux-arm64.tgz
         tar c -C ./dist/linux_arm64 ./lib/ollama/cuda_jetpack5  | pigz -9vc >./dist/ollama-linux-arm64-jetpack5.tgz
         tar c -C ./dist/linux_arm64 ./lib/ollama/cuda_jetpack6  | pigz -9vc >./dist/ollama-linux-arm64-jetpack6.tgz
-        tar c -C ./dist/linux_amd64 --exclude sycl,rocm . | pigz -9vc >./dist/ollama-linux-amd64.tgz
+        tar c -C ./dist/linux_amd64 --exclude rocm --exclude sycl . | pigz -9vc >./dist/ollama-linux-amd64.tgz
         tar c -C ./dist/linux_amd64 ./lib/ollama/rocm  | pigz -9vc >./dist/ollama-linux-amd64-rocm.tgz
         tar c -C ./dist/linux_amd64 ./lib/ollama/sycl  | pigz -9vc >./dist/ollama-linux-amd64-sycl.tgz
 elif echo $PLATFORM | grep "arm64" > /dev/null ; then
@@ -52,6 +52,6 @@ elif echo $PLATFORM | grep "arm64" > /dev/null ; then
         tar c -C ./dist/ ./lib/ollama/cuda_jetpack5  | pigz -9vc >./dist/ollama-linux-arm64-jetpack5.tgz
         tar c -C ./dist/ ./lib/ollama/cuda_jetpack6  | pigz -9vc >./dist/ollama-linux-arm64-jetpack6.tgz
 elif echo $PLATFORM | grep "amd64" > /dev/null ; then
-        tar c -C ./dist/ --exclude sycl,rocm bin lib | pigz -9vc >./dist/ollama-linux-amd64.tgz
+        tar c -C ./dist/ --exclude rocm --exclude sycl bin lib | pigz -9vc >./dist/ollama-linux-amd64.tgz
         tar c -C ./dist/ ./lib/ollama/sycl  | pigz -9vc >./dist/ollama-linux-amd64-sycl.tgz
 fi
